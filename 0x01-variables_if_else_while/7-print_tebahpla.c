972078e8fe3b3f19181f5e2ce42ea0cc97024eb3
santiago <4547@holbertonschool.com>
2022-02-11 12:06:52 -0800

    de la z hasta la a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char p;
+
+for (p = 'z'; p <= 'a'; p--)
+putchar(p);
+putchar('\n');
+
+return (0);
+}
+
