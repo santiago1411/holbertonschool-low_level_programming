038dc0ab8efa2ddb768d285252b44ed055104a1a
santiago <4547@holbertonschool.com>
2022-02-11 11:39:15 -0800

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int base;
+
+for (base = 0; base < 10, base++)
+printf("%d", base);
+putchar('\n');
+
+return (0);
+}
