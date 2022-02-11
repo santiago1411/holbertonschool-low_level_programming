20241bd207d1e73a12b839cb5b761174a7290a80
santiago <4547@holbertonschool.com>
2022-02-11 11:14:44 -0800

    putchar M

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char p;
+
+for (p = 'a'; p <= 'z'; p++)
+putchar(p);
+for (p = 'A'; p <= 'Z'; p++)
+putchar(p);
+putchar('\n');
+
+
+return (0);
+}
