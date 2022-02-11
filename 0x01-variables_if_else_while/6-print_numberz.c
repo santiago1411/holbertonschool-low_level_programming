5e1f340c41e818b128358812ff2b49800b8f0894
santiago <4547@holbertonschool.com>
2022-02-11 11:51:07 -0800

    base 10 con putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int base;
+
+for (base = 0; base < 10; base++)
+putchar(base);
+putchar('\n');
+
+return (0);
+}
+
