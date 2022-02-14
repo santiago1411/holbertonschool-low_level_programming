d7beed40108a424fb066f0a653898788a08a71e8
santiago <4547@holbertonschool.com>
2022-02-14 12:38:48 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+else
+	{	
+		return (0);
+}
+_putchar('\n');
+}
