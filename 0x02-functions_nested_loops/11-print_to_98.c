3c995bdc1b57f373b53997e09de36d5418c76c97
santiago <4547@holbertonschool.com>
2022-02-15 10:47:39 -0800

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_to_98 - print numbers n to 98
+ * @n: integer argument
+ */
+void print_to_98(int n)
+{
+	for (n == 0; n <= 98; n++)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+}
