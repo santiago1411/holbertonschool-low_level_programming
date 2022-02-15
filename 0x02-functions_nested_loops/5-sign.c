66140600b66c680f4881966188cd914395a3a5ba
santiago <4547@holbertonschool.com>
2022-02-15 06:56:17 -0800

    sing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_sign - print sign of number
+ * @n: number to check
+ * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		 return (1);
+	return (1);
 	}
 	else if (n == 0)
-	{	
+	{
 		_putchar('0');
-		 return (0);
+	return (0);
 	}
 	else
 	{
 			_putchar('-');
-			 return (-1);
+	return (-1);
 	}
 	_putchar('\n');
 }
