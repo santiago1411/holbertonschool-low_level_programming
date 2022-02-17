650df1115be2bab1e02f5899b5edb7d982a861aa
santiago <4547@holbertonschool.com>
2022-02-17 11:18:12 -0800

    _

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line -  checks for checks for a digit (0 through 9).
+ * @n: n -  Variable
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	char c
+
+		for (c = 0; c <= n; c++)
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
+}
