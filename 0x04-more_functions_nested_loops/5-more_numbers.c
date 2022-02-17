6ca6ab30532141a5a90380e6cd0f0820cad153a5
santiago <4547@holbertonschool.com>
2022-02-17 09:30:49 -0800

    not print

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "holberton.h"
+
+/**
+ * more_numbers -  checks for checks for a digit (0 through 9).
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c < 15; c++)
+			{
+				if (c > 9)
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+			}
+		_putchar('\n'}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
-void print_most_numbers(void);
+void print_most_numbers
+void more_numbers(void);
 
 #endif
