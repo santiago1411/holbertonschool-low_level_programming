c34b7697b563050ca1e914e67ce2cc7b4cf4223c
santiago <4547@holbertonschool.com>
2022-02-17 11:28:49 -0800

    caracter

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_diagonal -  checks for checks for a digit (0 through 9).
+ * @n: n -  Variable
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	char c;
+
+		for (c = 0; c < n; c++)
+		{
+			_putchar('\');
+		}
+	_putchar('\n');
+}
