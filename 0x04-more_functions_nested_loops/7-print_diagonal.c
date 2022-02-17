2ea09bc7b40844f9be46d1c26feffb2eaaf5f39d
santiago <4547@holbertonschool.com>
2022-02-17 11:47:21 -0800

    caracter

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-	{
+		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j <= i; j++)
@@ -25,7 +25,5 @@ void print_diagonal(int n)
 				_putchar(' ');
 			}
 		}
-	}
-	_putchar('\n');
 	}
 }
