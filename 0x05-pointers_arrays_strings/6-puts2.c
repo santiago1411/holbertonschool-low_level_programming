83a58f6843eb6a026b6c39ad006db74c237fe766
santiago <4547@holbertonschool.com>
2022-02-21 14:40:18 -0800

    reverse

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,16 +8,16 @@ void puts2(char *str)
 {
 	int longi = 0;
 	int t = 0;
-	char y = str;
+	char *y = str;
 	int o;
 
-	while (y != '\0')
+	while (*y != '\0')
 	{
 		y++;
 		longi++;
 	}
 	t = longi - 1;
-	for (o = 0 ; o <= l ; o++)
+	for (o = 0 ; o <= t ; o++)
 	{
 	if (o % 2 == 0)
 	{
