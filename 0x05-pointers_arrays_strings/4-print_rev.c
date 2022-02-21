515a3befa17d5d5456436c19179d4fa3fed69d15
santiago <4547@holbertonschool.com>
2022-02-21 13:07:40 -0800

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,11 +12,13 @@ void print_rev(char *s)
 	while (*s != '\0')
 	{
 		longi++;
-		s++;
+		++s;
 	}
 	for (o = longi; o >= 0; o--)
 	{
-		putchar(*s);
+		_putchar(*s);
+		s--;
 	}
-	putchar('\n');
+
+	_putchar('\n');
 }
