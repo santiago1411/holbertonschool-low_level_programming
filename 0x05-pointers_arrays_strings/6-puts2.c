b1fadda7441661d71c2a88d5300b521367f6bec1
santiago <4547@holbertonschool.com>
2022-02-21 14:38:30 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char y = str;
+	int o;
+
+	while (y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= l ; o++)
+	{
+	if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
+}
