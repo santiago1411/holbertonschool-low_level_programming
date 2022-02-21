f915a9555c99f34b444e718cffb713867e1251a1
santiago <4547@holbertonschool.com>
2022-02-21 11:50:14 -0800

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+int string;
+
+while (*s != '\0')
+{
+	string++;
+	s++;
+}
+
+return (string);
+}
