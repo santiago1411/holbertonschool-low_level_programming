ee6226772fcf918124a171af7e85f7478c470971
santiago <4547@holbertonschool.com>
2022-02-21 12:41:16 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	for (o = longi; o > 0; --o)
+	{
+		putchar(--o);
+	}
+	putchar(\n);
+}
