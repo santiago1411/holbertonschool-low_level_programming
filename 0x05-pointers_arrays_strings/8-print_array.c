c4b6985c37727c7fb1791fffa9b658bcc9559a7f
santiago <4547@holbertonschool.com>
2022-02-21 15:11:32 -0800

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,8 +11,12 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < (n - 1); i++)
+	{
 		printf("%d, ", a[i]);
+	}
 		if (i == (n - 1))
+		{
 			printf("%d", a[n - 1]);
-	printf("\n");
+		}
+			printf("\n");
 }
