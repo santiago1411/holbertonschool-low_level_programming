89082aaf8d1d5e8d97d8a1d29681dc795541ab6f
santiago <4547@holbertonschool.com>
2022-02-21 12:13:18 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#includes "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		+str++;
+	}
+	puts('\n');
+}
