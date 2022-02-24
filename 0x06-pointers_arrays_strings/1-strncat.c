0961d3be7f68e087cec716124b285e04888d6293
santiago <4547@holbertonschool.com>
2022-02-24 12:31:46 -0800

    n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 	{
-	while (dest [i] != '\0')
+	while (dest[i] != '\0')
 		i++
 		{
 			while (src[j] != '\0')
@@ -17,5 +17,3 @@ char *_strncat(char *dest, char *src, int n)
 	dest[i] = '\0';
 	return (dest);
 }
-
-
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,15 +2,17 @@
 #define MAIN.H
 
 int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_numbers(void);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
+char *rot13(char *a);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
