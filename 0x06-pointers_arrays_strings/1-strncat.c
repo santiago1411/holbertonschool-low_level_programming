00a54bb2aaa169d9addc1f94e3acdd88c7acf97b
santiago <4547@holbertonschool.com>
2022-02-24 12:41:44 -0800

    n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,13 +18,13 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-		j = 0;
-		while (j < n && src[j] != '\0')
-		{
-				dest[i] = src[j];
-				i++;
-				j++;
-		}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
 	dest[i] = '\0';
 	return (dest);
 }
