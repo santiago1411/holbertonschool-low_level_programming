7f9f3a024160f5da4f9132e0e9fe233aea09be41
santiago <4547@holbertonschool.com>
2022-02-28 10:54:22 -0800

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
