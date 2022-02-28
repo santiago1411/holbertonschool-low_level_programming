33897c95fa00c1fca7fd2d03abddcfc8cada45dd
santiago <4547@holbertonschool.com>
2022-02-28 08:22:06 -0800

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return(dest);
+}
