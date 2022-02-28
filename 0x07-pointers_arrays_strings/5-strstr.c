f6789b4df86bcf32bcb90403306559a385fd1fac
santiago <4547@holbertonschool.com>
2022-02-28 10:56:33 -0800

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
@@ -12,7 +17,7 @@ char *_strstr(char *haystack, char *needle)
 			l++;
 			p++;
 		}
-		
+
 		if (*p == '\0')
 			return (haystack);
 	}
