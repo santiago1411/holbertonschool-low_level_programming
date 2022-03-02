57d344de4c577eaf647b7f61b87d48302f865ceb
santiago <4547@holbertonschool.com>
2022-03-02 11:59:03 -0800

    returns the value of x raised to the power of y

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- * factorial - youtube c tutorial: 5 - (5 - 1)
  * @n: input
  * Return: Always 0 (Success)
  */
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,6 +9,8 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
+	if (y == 0)
+		return (1)
 	else 
 		return (x * _pow_recursion(x, y - 1));
 }
