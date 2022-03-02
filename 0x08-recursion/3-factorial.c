c3df3fa5e6d4f89690c05727b4d83093964007fc
santiago <4547@holbertonschool.com>
2022-03-02 11:40:02 -0800

    the factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
+/**
+ * factorial - youtube c tutorial: 5 - (5 - 1)
+ * @n: input
+ * Return: Always 0 (Success)
+ */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (1);
-	else 
+	else
 		return (n * factorial(n - 1));
 }
