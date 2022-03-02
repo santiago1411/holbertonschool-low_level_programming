b07e5437aa0a69d3724384c9fea62f7bb9b3f01e
santiago <4547@holbertonschool.com>
2022-03-02 12:58:11 -0800

    returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-int sqrtroof(int n, int i)
+int squareroof(int n, int i)
 /**
  * _sqrt_recursion - no loop, 1. 2nd function - checks for perfect square
  * @n: input
@@ -10,20 +10,20 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else 
-		return (sqrtroof(n, (n + 1) / 2));
+		return (squareroof(n, (n + 1) / 2));
 }
 /**
- * sqrtroof - checks if perfect square
+ * squareroof - checks if perfect square
  * @n: input
  * @i: counter
  * Return: if square root
  */
-int sqrtroof(int n, int i)
+int squareroof(int n, int i)
 {
 	if (i < 1)
 		return (-1);
 	else if (i * i == n)
 		return (i);
 	else 
-		return (sqrtroof(n, i - 1));
+		return (squareroof(n, i - 1));
 }
