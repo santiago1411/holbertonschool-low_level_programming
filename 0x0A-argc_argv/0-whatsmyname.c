d236e239b3b824e30bf0effce85764b154acea90
santiago <4547@holbertonschool.com>
2022-03-04 09:30:50 -0800

    prints the number of arguments passed into it

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,8 @@
  *
  * Return: Always o.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	argc = argc;
 	Printf("%s\n", *argv[0]);
 
 	return (0);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
 
 #endif
