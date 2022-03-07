0707fddef38e8a5c1a61377de3bdd5537745d943
santiago <4547@holbertonschool.com>
2022-03-07 09:57:42 -0800

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+#include <stdlib.h>
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
