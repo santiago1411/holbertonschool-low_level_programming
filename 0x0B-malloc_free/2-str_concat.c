b58aa451c938bbfed4792a1f0f1e805e5e1a19f1
santiago <4547@holbertonschool.com>
2022-03-07 10:07:55 -0800

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,19 +25,17 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	
 	i = ci = 0;
-	while (*s1)
+	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
 		i++;
-		s1++;
 	}
 
-	while (*s2)
+	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
 		i++;
 		ci++;
-		s2++;
 	}
 	conct[i] = '\0';
 	return (conct);
