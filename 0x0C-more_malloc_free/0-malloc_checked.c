eeecf0db6c3246ee0737f42e67912e6ba627534b
santiago <4547@holbertonschool.com>
2022-03-11 06:26:33 -0800

    More malloc, free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - Entry Point
+ * @b: input amount
+ * Return: pointer to new mem
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *dew;
+	
+	dew = malloc(b);
+	if (dew == NULL)
+		exit(98);
+	return (dew);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		arr[i] = 0;
+
+	return (arr);
+}
