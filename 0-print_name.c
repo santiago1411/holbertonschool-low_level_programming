5ab03846cadb4ed8c6d2dacc46cfc37a78df3067
santiago <4547@holbertonschool.com>
2022-03-17 07:08:39 -0700

    holberton_low_level

--- 0-print_name.c
+++
@@ -1,6 +0,0 @@
-#include "function_pointers.h"
-
-void print_name(char *name, void (*f)(char *))
-{
-	f(name);
-}
--- function_pointers.h
+++
@@ -1,5 +0,0 @@
-#ifndef function_pointers.h
-#define function_pointers.h
-
-int _putchar(char c):
-void print_name(char *name, void (*f)(char *));
