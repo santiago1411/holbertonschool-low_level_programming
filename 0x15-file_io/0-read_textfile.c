d6fc301993cccc8ed017a3d89d2f44622edb572f
santiago <4547@holbertonschool.com>
2022-04-05 10:24:02 -0700

    read, write and create

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY, 0600);
-	if (fd = -1)
+	if (fd == -1)
 		return (0);
 
 	lectura = read(fd, buff, letters);
