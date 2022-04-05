96d5866947a26780950c939e3debaa2496de4a4f
santiago <4547@holbertonschool.com>
2022-04-05 11:53:20 -0700

    Write a program that copies the content of a file to another file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/uio.h>
@@ -9,9 +10,7 @@
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void copy_file(const char *src, const char *dest);
 
 #endif
