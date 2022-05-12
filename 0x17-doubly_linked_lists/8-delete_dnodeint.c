613ec84ec8f141256b7dba4af55af6ff290658ca
santiago1411 <4547@holbertonschool.com>
2022-05-12 14:37:00 -0500

    Write a function that deletes the node at index index of a dlistint_t linked list.

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,40 +1,48 @@
 #include "lists.h"
-
 /**
- * delete_dnodeint_at_index - delete node at index
- * @head: double pointer to head of node
- * @index: index to delete
- * Return: list with deleted node, 1 on success, -1 on fail
- */
+* delete_dnodeint_at_index - Delete node at index
+* @head: double pointer
+* @index: int
+* Return: -1 if error or 1 on succed
+*/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int count = 0;
-	dlistint_t *temp = NULL;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-	if (head == NULL || *head == NULL)
-		return (-1);
+	h1 = *head;
 
-	temp = *head;
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
 
-	if (index == 0)
+	i = 0;
+
+	while (h1 != NULL)
 	{
-		*head = (*head)->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(temp);
-		return (1);
-	}
-	while (temp->next != NULL)
-	{
-		if (count == index)
+		if (i == index)
 		{
-			temp->next->prev = temp->prev;
-			temp->prev->next = temp->next;
-			free(temp);
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+
+			free(h1);
 			return (1);
 		}
-		temp = temp->next;
-		count++;
+		h2 = h1;
+		h1 = h1->next;
+		i++;
 	}
 
 	return (-1);
