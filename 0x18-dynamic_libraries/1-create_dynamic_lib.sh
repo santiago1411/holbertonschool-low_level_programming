ebc9e60612c58b234cd4652c221adb8c4e12df23
santiago1411 <4547@holbertonschool.com>
2022-05-16 12:03:39 -0500

    libreria dinamica

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,2 @@
+gcc -fPIC -c *.c
+gcc *.o -shared -o liball.so
---
+++ 0x18-dynamic_libraries/cfiles/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _atoi - tbd
+ *
+ * @s: parameter defined in main, pointer to memory
+ *
+ * Return: 0 for now
+ */
+
+int _atoi(char *s)
+{
+	s = s + 1;
+	return (0);
+}
