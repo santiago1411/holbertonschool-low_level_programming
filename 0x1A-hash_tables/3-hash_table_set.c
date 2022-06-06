571d21f2eb87d28b227084e23bea96eed4db2cf6
santiago1411 <4547@holbertonschool.com>
2022-06-06 17:53:33 -0500

    add new element

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,41 +1,40 @@
 #include "hash_tables.h"
+#include <string.h>
 /**
  * hash_table_set - function that adds an element to the hash table.
- *
- * @key: key
- * @ht: hash table
- * @value: value to enter
- *
- * Return: index of the key
+ * @ht: is the hash table you want to add or update the key/value to
+ * @key: key inside the hash table - can not be an empty string
+ * @value: value to pair with key - must be duplicated - can be an empty string
+ * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node = NULL;
-	hash_node_t *temp = NULL;
-	unsigned int index = 0;
+	hash_node_t *aux = NULL;
+	unsigned long int index = 0;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	temp = ht->array[index];
+	aux = ht->array[index];
 
-	while (temp != NULL)
+	while (aux != NULL)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (strcmp(aux->key, key) == 0)
 		{
-			free(temp->value);
-			temp->value = strdup(value);
+			free(aux->value);
+			aux->value = strdup(value);
 			return (1);
+
 		}
-		temp = temp->next;
+		aux = aux->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-
-	new_node->key = strdup(key);
 	new_node->value = strdup(value);
+	new_node->key = strdup(key);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
