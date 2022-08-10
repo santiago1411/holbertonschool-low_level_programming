29a94880ff22cb30481ae9fda8b74474252ad82e
santiago1411 <4547@holbertonschool.com>
2022-08-10 09:40:10 -0500

    big O, searching, worst cases

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,11 +12,6 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (array == NULL)
-	{
-		return (-1);
-	}
-
 	while (i < size)
 	{
 		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
